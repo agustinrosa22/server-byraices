59bb46cd307e162c6217a6fd961dfff194206eef
agustinrosa22 <agustinrosa1234@gmail.com>
2024-12-02 13:07:17 -0300

    cambios modelo visitas

--- src/models/Visita.js
+++ src/models/Visita.js
@@ -25,6 +25,68 @@ module.exports = (sequelize) => {
       type: DataTypes.TEXT,  // Texto más largo para la descripción
       allowNull: true,  // Puede ser opcional
     },
+    gusto: {
+      type: DataTypes.JSON, // Usamos JSON para guardar yes y no
+      allowNull: false,
+      defaultValue: {
+        yes: false,
+        no: false,
+      },
+    },
+    calificacionUbicacion: {
+      type: DataTypes.JSON, // Usamos JSON para guardar las opciones de calificación
+      allowNull: false,
+      defaultValue: {
+        excelente: false,
+        buena: false,
+        regular: false,
+        mala: false,
+      },
+    },
+    espaciosYComodidades: {
+      type: DataTypes.JSONB,
+      defaultValue: {
+        muySatisfactorio: false,
+        satisfactorio: false,
+        insatisfactorio: false
+      }
+    },
+    calidadPrecio: {
+      type: DataTypes.JSONB,
+      defaultValue: {
+        excelente: false,
+        buena: false,
+        regular: false,
+        mala: false,
+      }
+    },
+    general: {
+      type: DataTypes.JSONB,
+      defaultValue: {
+        excelente: false,
+        muyBuena: false,
+        buena: false,
+        regular: false,
+        mala: false,
+      }
+    },
+    comprar: {
+      type: DataTypes.JSON, // Usamos JSON para guardar yes y no
+      allowNull: false,
+      defaultValue: {
+        yes: false,
+        no: false,
+      },
+    },
+    comprar: {
+      type: DataTypes.JSON, // Usamos JSON para guardar yes y no
+      allowNull: false,
+      defaultValue: {
+        yes: false,
+        no: false,
+        maybe: false,
+      },
+    },
   });
 
   return Visita;
