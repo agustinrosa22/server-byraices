24c1daf89573f765fdad54168272dd97b92ef3bb
agustinrosa22 <agustinrosa1234@gmail.com>
2024-12-05 12:33:26 -0300

    ,

--- src/controllers/getActivePropertiesForSale.js
+++ src/controllers/getActivePropertiesForSale.js
@@ -8,7 +8,7 @@ const getActivePropertiesForSale = async (req, res) => {
     let conditions = {
       statusProperty: true,
       isForSale: true,
-      'cerrado,cierre': true
+      'cerrado.cierre': true
     };
 
     if (province) {
